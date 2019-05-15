41ade1be3938c2d3ccfe4d663a1477228d9cebbe
David Hamelin <hamelindavid@hotmail.fr>
2019-05-15 10:20:55 +0200

    hanami sushi bar

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using hanamiSushiBar.Models;
+using System.Data.Entity;
+
+namespace hanamiSushiBar.Controllers
+{
+    public class HomeController : Controller
+    {
+        private sushiBarEntities db = new sushiBarEntities();
+
+        public ActionResult Index()
+        {
+            var products = db.product.Include(prod => prod.categories);
+            return View(products.ToList());
+        }
+        
+        
+    }
+}
